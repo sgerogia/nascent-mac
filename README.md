4dbea1bbddd0d435e995374ccbd9f7e3e65f695a
Stelios Gerogiannakis <sgerogia@gmail.com>
2017-07-04 23:00:18 +0100

    Reworked Ansible. Stable OSX setup

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # nascent-mac
+
 Mac gone? No problem!
+
+## Running
+
+```bash
+cd ~
+curl -L -o setup.sh https://github.com/sgerogia/nascent-mac/raw/master/setup.sh
+sudo ./setup.sh
+```
--- setup.sh
+++ setup.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env bash
 
-SRC_DIR=~/src/github.com
+SRC_DIR=~/src/github.com/sgerogia
 SETTINGS_PROJECT=nascent-mac
 
+echo "----> Updating XCode..."
 xcode-select --install || true
 
+sudo xcodebuild -license accept
+
+echo "----> Installing Ansible..."
 sudo easy_install pip
 sudo pip install ansible
 
+echo "----> Cloning project to ${SRC_DIR}..."
 mkdir -p ${SRC_DIR}
 cd ${SRC_DIR}
 
-# git clone "https://github.com/sgerogia/${SETTINGS_PROJECT}.git" || true
+git clone "https://github.com/sgerogia/${SETTINGS_PROJECT}.git" || true
 
-cd "${SETTINGS_PROJECT}/ansible"
+cd "${SRC_DIR}/${SETTINGS_PROJECT}/ansible"
 
+echo "----> Updating Mac. Hold on tight!..."
 ansible-galaxy install -r requirements.yml
 
-ansible-playbook -v desktop.yml
+ansible-playbook -v setup.yml
