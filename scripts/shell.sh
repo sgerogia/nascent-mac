9a13c38b20e9b464c83a0bf220d1282bc846293a
Stelios <sgerogia@gmail.com>
2024-06-12 10:51:57 +0400

    Add inputrc

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -15,6 +15,10 @@ source "$HOME/.sdkman/bin/sdkman-init.sh"
 echo "----> Replacing .bash_sgerogia"
 cp ../dotfiles/bash_sgerogia.local "/Users/sgerogia/.bash_sgerogia"
 
+echo "----> Replacing .inputrc"
+cp ../dotfiles/inputrc.local "/Users/sgerogia/.inputrc"
+
+
 if [[ ! -f "/private/etc/sudoers.d/mysudo" ]]; then
   echo "----> Enabling passwordless sudo"
   sudo bash -c 'echo "sgerogia            ALL = (ALL) NOPASSWD: ALL" > "/private/etc/sudoers.d/mysudo"'
